a88c6284ecb64f003aed5c29a9ddf0a39359803e
Mike Arango <maicol@Macbooks-MacBook-Pro.local>
2023-01-22 21:21:28 -0500

    Modificacion de AgregarDirectrizMentoriaUseCase y creacion de AgregarDirectrizMentoriaUseCaseTest check

--- Entrenamiento-CasoUso-main/business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCase.java
+++ Entrenamiento-CasoUso-main/business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCase.java
@@ -11,10 +11,10 @@ public class AgregarDirectrizMentoriaUseCase extends UseCase<RequestCommand<Agre
     @Override
     public void executeUseCase(RequestCommand<AgregarDirectrizMentoria> agregarDirectrizMentoriaRequestCommand) {
         var command = agregarDirectrizMentoriaRequestCommand.getCommand();
-        var curso = Curso.from(
-                command.getCursoId(), repository().getEventsBy(command.getCursoId().value())
-        );
-        curso.agregarDirectrizMentoria(command.getMentoriaId(),command.getDirectiz());
+        var curso = Curso.from(command.getCursoId(), repository().getEventsBy(command.getCursoId().value()));
+        curso.agregarDirectrizDeMentoria(
+                command.getMentoriaId(),
+                command.getDirectiz());
         emit().onResponse(new ResponseEvents(curso.getUncommittedChanges()));
 
     }
